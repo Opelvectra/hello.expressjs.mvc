f9c2de3c2d878d649e7e04fd7a3064ebbe22fdec
Dore Mioto <dore.mioto@gmail.com>
2017-09-13 00:40:14 +0300

    added timeline controller for react project

--- hello.expressjs.mvc/controllers/friends/f2.js
+++ hello.expressjs.mvc/controllers/friends/f2.js
@@ -1,12 +1,27 @@
 var someService = require('./../../services/cars/s2');
+var data = [];
+for(var i = 1; i < 36; i++){
+	data.push({
+		imageUrl: 's/image/' + i + '.jpg',
+		description: 'This is description of Dog type #' + i + '. These dogs are.. bla bla bla.. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+			'Aliquam vitae rhoncus ipsum, et elementum diam. Fusce sollicitudin velit in felis lacinia ullamcorper. Pellentesque nec felis orci. ' + 
+			'Suspendisse laoreet nibh non ornare sollicitudin. Quisque mi orci, bibendum id est eu, finibus vulputate nisi. Duis non libero quis ' + 
+			'ligula congue lacinia. Nam nec mattis metus, lacinia sollicitudin augue. Fusce in tellus varius, molestie justo a, efficitur ipsum. ' + 
+			'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque habitant morbi tristique senectus...',
+		title: 'Dog type #' + i,
+		id: i,
+		price: 500
+	});
+}
 
 module.exports = {
-	path: '/f2',
+	path: '/timeline',
 	get: function(req, res) {
 		console.log('someService: ' + someService.about());
-		res.render('a', {
-			xxx: 'YyY: Friend_2(get)'
-		});
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify({ 
+	    	topics: data
+	    }));
 	},
 	post: function(req, res) {
 		res.render('a', {
--- hello.expressjs.mvc/interceptors/inter-1.js
+++ hello.expressjs.mvc/interceptors/inter-1.js
@@ -1,4 +1,5 @@
 module.exports = function (req, res, next) {
 	console.log('>> inter 1');
+	res.header("Access-Control-Allow-Origin", "*");
 	next();
 };
